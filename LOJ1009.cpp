29d5a64e971df87f0a3cd91503940208aa5ff68a
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-01-17 21:14:06 +0600

    Update LOJ1009.cpp

--- LOJ1009.cpp
+++ LOJ1009.cpp
@@ -1,17 +1,62 @@
 #include<bits/stdc++.h>
-using namespace std;
 
-#define ll long long int
+///...................................*****.................................................///
+///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+using namespace std;
+#define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define           ll               long long int
+#define           ull              unsigned long long int
+#define           pii              pair < int, int>
+#define           pll              pair < ll, ll>
+#define           MOD              1000000007
+#define           vi               vector<int>
+#define           vl               vector<ll>
+#define           pb               push_back
+#define           sc               scanf
+#define           pf               printf
+#define         scin(x)            scanf("%d",&(x))
+#define        scin2(x,y)          scanf("%d %d",&(x),&(y))
+#define         scln(x)            scanf("%lld",&(x))
+#define        scln2(x,y)          scanf("%lld %lld",&(x),&(y))
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       min4(a,b,c,d)        min(d,min(a,min(b,c)))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       max4(a,b,c,d)        max(d,max(a,max(b,c)))
+#define         ms(a,b)            memset(a,b,sizeof(a))
+#define           mp               make_pair
+#define       gcd(a, b)            __gcd(a,b)
+#define       lcm(a, b)            ((a)*(b)/gcd(a,b))
+#define         input              freopen("input.txt","rt", stdin)
+#define         output             freopen("output.txt","wt", stdout)
+#define           PI               2*acos(0.0)
+#define     rep( i , a , b )       for( i=a ; i<b ; i++)
+#define     rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define     repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define       CASEL(t)             printf("Case %d:\n",t)
+#define     RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
 
 #define white 0
 #define red 1
 #define blue 2
 
-vector<ll > adj[100005];
+vl adj[100005];
 ll visited[100005];
 
-vector<ll>r;
-vector<ll>h;
+vl r,h;
 
 ll BFS(ll s)
 {
@@ -21,7 +66,6 @@ ll BFS(ll s)
     Q.push(s);
     visited[s]=red;
     r.push_back(s);
-    // cout<<"red "<<s<<endl;
 
     while(!Q.empty())
     {
@@ -40,22 +84,17 @@ ll BFS(ll s)
                 {
                     visited[v]=blue;
                     h.push_back(v);
-                    //   cout<<"blue "<<v<<endl;
                 }
                 else
                 {
                     visited[v]=red;
                     r.push_back(v);
-                    //  cout<<"red "<<v<<endl;
                 }
             }
         }
     }
-    // cout<<r.size()<<" "<<h.size()<<endl;
+
     ll x=max(r.size(),h.size());
-    // cout<<x<<endl;
-    r.clear();
-    h.clear();
     return x;
 }
 
@@ -63,34 +102,32 @@ int main()
 {
     ll t,a,b,s;
     set <ll> st;
-    cin>>t;
+    scin(t);
     for(ll k=1; k<=t; k++)
     {
         memset(visited,white,sizeof visited);
         ll sum=0;
-        scanf("%lld",&s);
+        scln(s);
         for(ll i=0; i<s; i++)
         {
-            scanf("%lld %lld",&a,&b);
+            scln2(a,b);
             adj[a].push_back(b);
             adj[b].push_back(a);
             st.insert(a);
             st.insert(b);
         }
 
-        set < ll > :: iterator it;
 
-        for(it = st.begin(); it != st.end(); it++)
+
+        for(auto x : st)
         {
-            //cout<<*it<<" ";
-            if(visited[*it]==white)
+            if(visited[x]==white)
             {
-                //cout<<*it<<" ";
-                ll f=BFS(*it);
+                ll f=BFS(x);
                 sum=sum+f;
             }
         }
-        printf("Case %lld: %lld\n",k,sum);
+        pf("Case %lld: %lld\n",k,sum);
 
 
 
@@ -103,6 +140,8 @@ int main()
 
     return 0;
 }
+
+
 /*
 final:
 4
