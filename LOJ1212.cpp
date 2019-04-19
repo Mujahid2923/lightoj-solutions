e0dd23509bcac2bd3237f538c413f41a49cffb19
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-04-19 06:16:37 +0600

    Add files via upload

---
+++ LOJ1212.cpp
@@ -0,0 +1,132 @@
+///...................................*****.................................................///
+///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define           ll               long long int
+#define           ull              unsigned long long int
+#define           pii              pair < int, int>
+#define           pll              pair < ll, ll>
+#define           MOD              1000000007
+#define           vi               vector<int>
+#define           vl               vector<ll>
+#define           pb               push_back
+#define           sc               scanf
+#define           pf               printf
+#define         scin(x)            scanf("%d",&(x))
+#define        scin2(x,y)          scanf("%d %d",&(x),&(y))
+#define         scln(x)            scanf("%lld",&(x))
+#define        scln2(x,y)          scanf("%lld %lld",&(x),&(y))
+#define       min3(a,b,c)          min(a,min(b,c))
+#define       min4(a,b,c,d)        min(d,min(a,min(b,c)))
+#define       max3(a,b,c)          max(a,max(b,c))
+#define       max4(a,b,c,d)        max(d,max(a,max(b,c)))
+#define         ms(a,b)            memset(a,b,sizeof(a))
+#define           mp               make_pair
+#define       gcd(a, b)            __gcd(a,b)
+#define       lcm(a, b)            ((a)*(b)/gcd(a,b))
+#define         input              freopen("input.txt","rt", stdin)
+#define         output             freopen("output.txt","wt", stdout)
+#define           PI               3.141592653589793238462643
+#define     rep( i , a , b )       for( i=a ; i<b ; i++)
+#define     rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define     repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define       CASEL(t)             printf("Case %d:\n",t)
+#define     RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+#define        zero(a)             memset(a,0,sizeof a)
+
+///priority_queue<int,vector<int>,greater<int> >pq;
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+
+
+int main()
+{
+    //output;
+    int t,n,qer,b;
+    string s;
+    cin>>t;
+    deque<int>q;
+    for(int k=1; k<=t; k++)
+    {
+        cin>>n>>qer;
+        pf("Case %d:\n",k);
+        for(int i=0; i<qer; i++)
+        {
+            cin>>s;
+            if(s=="pushLeft")
+            {
+                cin>>b;
+                if(q.size()==n)
+                {
+                    pf("The queue is full\n");
+                }
+                else
+                {
+                    q.push_front(b);
+                    pf("Pushed in left: %d\n",b);
+                }
+
+            }
+            else if(s=="pushRight")
+            {
+                cin>>b;
+                if(q.size()==n)
+                {
+                    pf("The queue is full\n");
+                }
+                else
+                {
+                    q.push_back(b);
+                    pf("Pushed in right: %d\n",b);
+                }
+            }
+            else if(s=="popLeft")
+            {
+                if(q.empty())
+                {
+                    pf("The queue is empty\n");
+                }
+                else
+                {
+                    int c=q.front();
+                    q.pop_front();
+                    pf("Popped from left: %d\n",c);
+                }
+            }
+            else if(s=="popRight")
+            {
+                if(q.empty())
+                {
+                    pf("The queue is empty\n");
+                }
+                else
+                {
+                    int c=q.back();
+                    q.pop_back();
+                    pf("Popped from right: %d\n",c);
+                }
+            }
+        }
+        while(!q.empty())
+        {
+            q.pop_front();
+        }
+    }
+    return 0;
+}
+
+
+
