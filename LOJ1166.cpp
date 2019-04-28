cca71ef047c24358a2699027d43c45e20e0a631f
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-04-28 23:23:03 +0600

    Update LOJ1166.cpp

--- LOJ1166.cpp
+++ LOJ1166.cpp
@@ -11,7 +11,7 @@ using namespace std;
 using namespace __gnu_pbds;
 
 #define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define       ordered_set                tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define       ordered_set          tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 #define           ll               long long int
 #define           ull              unsigned long long int
 #define           pii              pair < int, int>
