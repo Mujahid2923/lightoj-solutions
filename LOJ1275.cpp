46a6642873888ac5602926a398278fbdb4adf758
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-08-03 21:12:07 +0600

    Add files via upload

---
+++ LOJ1275.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define          FaRaBi                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ld                     long double
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          ff                     first
+#define          ss                     second
+#define          pb                     push_back
+#define          output                 freopen("output.txt","wt", stdout)
+#define          pf                     printf
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          PI                     acos(-1.0)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          Max(v)                 *max_element(all(v))
+#define          Min(v)                 *min_element(all(v))
+#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+#define          no                     cout << "NO" << endl ;
+#define          yes                    cout << "YES" << endl ;
+#define          segment_tree           int Lnode = node << 1 , Rnode = Lnode + 1 , mid = ( b + e ) >> 1 ;
+
+///--------------------**********----------------------------------
+
+vector < ll > v, v1, v2, v3, v4 ;
+vector < pair < ll, pll > > vec1, vec2 ;
+vector < pll > vec, Vec ;
+vector < ll > adj[ 100005 ] ;
+map < ll, ll > Mp, Mp1, Mp2 ;
+set < ll > st, st1, st2 ;
+queue < ll > Q ;
+stack < ll > Stk ;
+multiset < ll > S ;
+vector < string > vs ;
+
+
+///---------------------**********--------------------------------
+
+ll n, c, T ;
+
+void Reset()
+{
+
+}
+
+void Input()
+{
+    scanf( "%lld", &n ) ;
+    scanf( "%lld", &c ) ;
+}
+
+ll bsearch( ll low,ll high )
+{
+    ll res = 0, profit = 0 ;
+    while( low < high )
+    {
+        ll mid = ( low + high ) >> 1 ;
+        ll val = mid * ( c - ( mid * n ) ) ;
+        if( val > profit )
+        {
+            res = mid ;
+            profit = val ;
+            low = mid + 1 ;
+        }
+        else
+        {
+            high = mid - 1 ;
+        }
+    }
+    return res ;
+}
+
+void Calculation()
+{
+    ll ans = 0 ;
+    if( n > 0 ) ans = bsearch( 0, ( c + n - 1 ) / n ) ;
+    printf( "Case %lld: %lld\n", T, ans ) ;
+}
+
+void solve()
+{
+    Reset() ;
+    Input() ;
+    Calculation() ;
+}
+
+int main()
+{
+    int t ;
+    scanf( "%d", &t ) ;
+    while( t-- )
+    {
+        T ++ ;
+        solve() ;
+    }
+    return 0 ;
+}
+
