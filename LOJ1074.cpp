119a9539d16dd3b2cb4276b6c5177c0eb98d0fd7
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-08-28 23:04:51 +0600

    Add files via upload

--- LOJ1074.cpp
+++ LOJ1074.cpp
@@ -1,128 +1,120 @@
 #include<bits/stdc++.h>
-
-///...................................*****.................................................///
-///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
-
 using namespace std;
-#define           CIN              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define           ll               long long int
-#define           ull              unsigned long long int
-#define           pii              pair < int, int>
-#define           pll              pair < ll, ll>
-#define           MOD              1000000007
-#define           vi               vector<int>
-#define           vl               vector<ll>
-#define           pb               push_back
-#define           sc               scanf
-#define           pf               printf
-#define         scin(x)            scanf("%d",&(x))
-#define        scin2(x,y)          scanf("%d %d",&(x),&(y))
-#define         scln(x)            scanf("%lld",&(x))
-#define        scln2(x,y)          scanf("%lld %lld",&(x),&(y))
-#define       min3(a,b,c)          min(a,min(b,c))
-#define       min4(a,b,c,d)        min(d,min(a,min(b,c)))
-#define       max3(a,b,c)          max(a,max(b,c))
-#define       max4(a,b,c,d)        max(d,max(a,max(b,c)))
-#define         ms(a,b)            memset(a,b,sizeof(a))
-#define           mp               make_pair
-#define       gcd(a, b)            __gcd(a,b)
-#define       lcm(a, b)            ((a)*(b)/gcd(a,b))
-#define         input              freopen("input.txt","rt", stdin)
-#define         output             freopen("output.txt","wt", stdout)
-#define           PI               2*acos(0.0)
-#define     rep( i , a , b )       for( i=a ; i<b ; i++)
-#define     rev( i , a , b )       for( i=a ; i>=b ; i--)
-#define     repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
-#define       CASEL(t)             printf("Case %d:\n",t)
-#define     RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
 
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
+#define          FaRaBi                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          output                 freopen("output.txt","wt", stdout)
+#define          ld                     long double
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          ff                     first
+#define          ss                     second
+#define          pb                     push_back
+#define          pf                     printf
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          PI                     acos(-1.0)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          Max(v)                 *max_element(all(v))
+#define          Min(v)                 *min_element(all(v))
+#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+#define          no                     cout << "NO" << endl ;
+#define          yes                    cout << "YES" << endl ;
+#define          segment_tree           int Lnode = node << 1 , Rnode = Lnode + 1 , mid = ( b + e ) >> 1 ;
+#define          siz                    205
+///--------------------**********----------------------------------
 
-vector<int>adj[1000];
-int visited[1000];
-int node,edge;
-int distance1[100];
-int distance2[100];
-int result[100];
+vector < ll > v, v1, v2, v3, v4 ;
+vector < pair < pll, ll > > vec ;
+vector < ll > adj[ siz ], cost[ siz ] ;
+map < ll, ll > Mp ;
+set < ll > st, st1, st2 ;
+stack < ll > Stk ;
+multiset < ll > S ;
 
-void BFS(int s,int level[])
+///---------------------**********--------------------------------
+
+ll n, m, q, x, y, test ;
+ll dist[ siz ], val[ siz ], visited[ siz ] ;
+void Reset()
 {
-    for(int i=0; i<node; i++)
+    zero( visited ) ;
+    for( int i = 1 ; i <= n ; i ++ ) adj[ i ].clear(), cost[ i ].clear() ;
+}
+
+void Input()
+{
+    scanf( "%lld", &n ) ;
+    for( int i = 1 ; i <= n ; i ++ ) scanf( "%lld", &val[ i ] ) ;
+
+    scanf( "%lld", &m ) ;
+    for( int i = 0 ; i < m ; i ++ )
     {
-        visited[i]=0;
-        level[i]=0;
+        scanf( "%lld %lld", &x, &y ) ;
+        adj[ x ].pb( y ) ;
+        ll z = val[ y ] - val[ x ] ;
+        cost[ x ].pb( z * z * z ) ;
     }
+}
 
-    queue<int>Q;
-    Q.push(s);
-    visited[s]=1;
-    level[s]=0;
-
-
-
-    while(!Q.empty())
+void Bellmanford( int s )
+{
+    for( int i = 1 ; i <= n ; i ++ ) dist[ i ] = INT_MAX ;
+    dist[ s ] = 0 ;
+    for( int i = 1 ; i <= n ; i ++ )
     {
-        int u=Q.front();
-        Q.pop();
-
-        for(int i=0; i<adj[u].size(); i++)
+        for( int j = 1 ; j <= n ; j ++  )
         {
-            int v=adj[u][i];
-            if(visited[v]==0)
+            ll u = j ;
+            for( int k = 0 ; k < adj[ u ].size() ; k ++ )
             {
-                visited[v]=1;
-                Q.push(v);
-                level[v]=level[u]+1;
+                ll v = adj[ u ][ k ] ;
+                ll w = cost[ u ][ k ] ;
+                if( dist[ u ] + w < dist[ v ] ) dist[ v ] = dist[ u ] + w ;
             }
         }
     }
 }
 
-int main()
+void dfs( int u )
 {
-    int s,d;
-    int t,a,b;
-    cin>>t;
-
-    for(int k=1; k<=t; k++)
-    {
-        cin>>node>>edge;
-        for(int i=0; i<edge; i++)
-        {
-            cin>>a>>b;
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-        }
-
-        cin>>s>>d;
-        BFS(s,distance1);
-        BFS(d,distance2);
-        int ans=-1;
-        for(int i=0;i<node;i++)
-        {
-            result[i]=distance1[i]+distance2[i];
-        }
-
-        sort(result,result+node);
-        ans=result[node-1];
-
-        printf("Case %d: %d\n",k,ans);
-
-        for(int i=0; i<node; i++)
-        {
-            adj[i].clear();
-        }
-    }
-    return 0;
+    visited[ u ] = 1 ;
+    for( auto v : adj[ u ] ) if( !visited[ v ] ) dfs( v ) ;
 }
 
+void Calculation()
+{
+    pf( "Case %lld:\n", ++ test ) ;
+    dfs( 1 ) ;
+    Bellmanford( 1 ) ;
+    scanf( "%lld", &q ) ;
+    while( q-- )
+    {
+        scanf( "%lld", &x ) ;
+        if( dist[ x ] < 3 || dist[ x ] == INT_MAX || !visited[ x ] ) pf( "?\n" ) ;
+        else pf( "%lld\n", dist[ x ] ) ;
+    }
+}
+
+void solve()
+{
+    Reset() ;
+    Input() ;
+    Calculation() ;
+}
+
+int main()
+{
+    int t ;
+    cin >> t ;
+    while( t-- )
+    {
+        solve() ;
+    }
+    return 0 ;
+}
