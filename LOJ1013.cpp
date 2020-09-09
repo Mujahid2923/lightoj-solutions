3e06c11adb76957ab9b2c8c96b4695b0e5de8301
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-09-09 14:44:16 +0600

    Update LOJ1013.cpp

--- LOJ1013.cpp
+++ LOJ1013.cpp
@@ -77,7 +77,7 @@ void Calculation()
 {
     lcs = Fun1( 0, 0 ) ;
     ll ans = n1 + n2 - lcs ;
-    pf( "Case %lld: %lld %lld\n", T, ans, Fun2( 0, 0, 0 ) ) ;
+    printf( "Case %lld: %lld %lld\n", T, ans, Fun2( 0, 0, 0 ) ) ;
 }
 
 
@@ -100,5 +100,34 @@ int main()
 
     return 0 ;
 }
+***********************---------------------------------- Iterative-------------------------------*********************
 
+void Calculation()
+{
+    for( int i = 1 ; i <= n1 ; i ++ )
+    {
+        for( int j = 1 ; j <= n2 ; j ++ )
+        {
+            if( s1[ i - 1 ] == s2[ j - 1 ] ) lcs[ i ][ j ] = 1 + lcs[ i - 1 ][ j - 1] ;
+            else lcs[ i ][ j ] = max( lcs[ i - 1 ][ j ], lcs[ i ][ j - 1 ] ) ;
+        }
+    }
+
+    for( int i = 0 ; i <= n1 ; i ++ )
+    {
+        for( int j = 0 ; j <= n2 ; j ++ )
+        {
+            if( i == 0 || j == 0 ) dp[ i ][ j ] = 1 ;
+            else if( s1[ i - 1 ] == s2[ j - 1 ] ) dp[ i ][ j ] = dp[ i - 1 ][ j - 1 ] ;
+            else
+            {
+                if( lcs[ i - 1 ][ j ] > lcs[ i ][ j - 1 ] ) dp[ i ][ j ] = dp[ i - 1 ][ j ] ;
+                else if( lcs[ i ][ j - 1 ] > lcs[ i - 1 ][ j ] ) dp[ i ][ j ] = dp[ i ][ j - 1 ] ;
+                else dp[ i ][ j ] = dp[ i - 1 ][ j ] + dp[ i ][ j - 1 ] ;
+            }
+        }
+    }
+
+    printf( "Case %lld: %lld %lld\n", T, n1 + n2 - lcs[ n1 ][ n2 ], dp[ n1 ][ n2 ] ) ;
+}
 
